8345be65cf1d1dec8e48d01f313544efdbc0a4d9
JavierSua <Javier.su.96@gmail.com>
2021-02-17 10:24:57 -0300

    dependencias de bootstrap y fontawesome agregadas

--- src/main.js
+++ src/main.js
@@ -2,6 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import {library} from '@fortawesome/fontawesome-svg-core'
+import {fas} from '@fortawesome/free-solid-svg-icons'
+import {far} from '@fortawesome/free-regular-svg-icons'
+import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome' 
+import { faFacebook } from '@fortawesome/free-brands-svg-icons'
+
+library.add(fas);
+library.add(far);
+library.add(faFacebook)
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 Vue.config.productionTip = false
 
