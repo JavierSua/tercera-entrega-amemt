f1adf41fd5176314dcefd84f6cd2752e4c2e41b6
JavierSua <Javier.su.96@gmail.com>
2021-02-22 19:32:27 -0300

    carrusel armado, faltan las imagenes

---
+++ src/components/Carrusel.vue
@@ -0,0 +1,58 @@
+<template>
+<div>
+    <b-carousel
+      id="carousel-1"
+      v-model="slide"
+      :interval="5000"
+      controls
+      indicators
+      background="#ababab"
+      img-width="1024"
+      img-height="480"
+      style="text-shadow: 1px 1px 2px #333;"
+      @sliding-start="onSlideStart"
+      @sliding-end="onSlideEnd"
+    >
+      <!-- Text slides with image -->
+      <b-carousel-slide
+        caption="Insecto"
+        text="Nulla vitae elit libero, a pharetra augue mollis interdum."
+        img-src="https://picsum.photos/1024/480/?image=52"
+      ></b-carousel-slide>
+
+      <b-carousel-slide
+        caption="Lorem ipsum"
+        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+        img-src="https://picsum.photos/1024/480/?image=52"
+      ></b-carousel-slide>
+
+      <b-carousel-slide
+        caption="Pinguinos"
+        text="Praesent commodo cursus magna, vel scelerisque nisl consectetur."
+        img-src="https://picsum.photos/1024/480/?image=52"
+      ></b-carousel-slide>
+
+      <b-carousel-slide
+        caption="Bosque"
+        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+        img-src="https://picsum.photos/1024/480/?image=52"
+      ></b-carousel-slide>
+
+      <b-carousel-slide
+        caption="Desierto"
+        text="Praesent commodo cursus magna, vel scelerisque nisl consectetur."
+        img-src="https://picsum.photos/1024/480/?image=52"
+      ></b-carousel-slide>
+
+    </b-carousel>
+
+  </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style lang="">
+
+</style>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 import 'bootstrap/dist/css/bootstrap.min.css'
+import "bootstrap-vue/dist/bootstrap-vue.css";
 import Vuex from 'vuex';
 
 
@@ -17,6 +19,8 @@ library.add(far);
 library.add(faFacebookSquare, faTwitterSquare, faInstagramSquare, faPinterestSquare)
 Vue.component('font-awesome-icon', FontAwesomeIcon);
 Vue.component('font-awesome-layers', FontAwesomeLayers);
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin)
 Vue.use(Vuex);
 
 Vue.config.productionTip = false
